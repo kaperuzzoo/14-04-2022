0c0ba4c90e4c850e30351c5eaba8a12220aec51a
david vargas <kaperuzzoo@gmail.com>
2022-04-14 15:34:06 +0200

    14/04/2022

---
+++ js/strings.js
@@ -0,0 +1,36 @@
+    let NickDeFornite ="Kaperuzø"
+    let ComoMeLlamanMisAmigos ="kape"
+    let CuandoMeMatanEnFornite =new String("****")
+    
+    console.log(NickDeFornite,ComoMeLlamanMisAmigos,CuandoMeMatanEnFornite)
+    console.log(
+    NickDeFornite.length,
+    ComoMeLlamanMisAmigos.length,
+    CuandoMeMatanEnFornite.length,)
+
+
+    let NombreDeMIPadre = "Mauricio"
+    let ApellidoDeMIPadre = "Vargas"
+
+    let ElNombreCompletoDeMIPadre ="Hola este es el nombre completo de mi padre" + nombre +""+apellido +"."
+    console.log(ElNombreCompletoDeMIPadre);
+
+    let ElNombreCompletoDeMiPadre2=`Hola este es el nombre completo de mi padre ${nombre} ${apellido}`;
+    console.log(ElNombreCompletoDeMiPadre2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
